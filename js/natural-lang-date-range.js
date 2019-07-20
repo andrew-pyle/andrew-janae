ff67cba8a879d2947a676aac4a60ba0ec8dd41ee
Andrew Pyle <andrew.pyle@icloud.com>
2019-07-20 10:43:20 -0500

    Refactor JS scripts to use ESM imports
    - Needs http server to properly download imported files

---
+++ js/natural-lang-date-range.js
@@ -0,0 +1,26 @@
+const MONTHS_IN_YEAR = 12
+const MILLISECS_IN_YEAR = 1000 * 60 * 60 * 24 * 365
+
+/**
+ * Returns a string describing the approximate time in years and months between two dates
+ * @param {number} startDate milliseconds since UNIX epoch of start date
+ * @param {number} endDate milliseconds since UNIX epoch of end date
+ */
+function getNaturalLanguageRange(startDate, endDate) {
+    const differenceInMillisecs = endDate - startDate
+    const differenceInYears = differenceInMillisecs / MILLISECS_IN_YEAR
+
+    const yearsSinceStartDate = Math.floor(differenceInYears)
+    const monthsRemainderDecimal = differenceInYears - yearsSinceStartDate
+    const monthsRemainderInteger = Math.round(MONTHS_IN_YEAR * monthsRemainderDecimal)
+
+    const roundedYearsSinceStartDate = monthsRemainderInteger < 12 ? yearsSinceStartDate : yearsSinceStartDate + 1
+    const roundedMonthsRemainderInteger = monthsRemainderInteger < 12 ? monthsRemainderInteger : null
+
+    const naturalLanguageString = `${roundedYearsSinceStartDate} years` + (roundedMonthsRemainderInteger ? ` & ${roundedMonthsRemainderInteger} months` : "")
+
+
+    return naturalLanguageString
+}
+
+export { getNaturalLanguageRange }
--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,8 @@
-const MONTHS_IN_YEAR = 12
-const MILLISECS_IN_YEAR = 1000 * 60 * 60 * 24 * 365
-const DATE_MARRIED = new Date(2016, 6, 22)
-const currentDate = new Date()
+import { getNaturalLanguageRange } from "./natural-lang-date-range.js";
 
-const differenceInMillisecs = currentDate - DATE_MARRIED
-const differenceInYears = differenceInMillisecs / MILLISECS_IN_YEAR
-
-const yearsSinceWedding = Math.floor(differenceInYears)
-const monthsRemainderDecimal = differenceInYears - yearsSinceWedding
-const monthsRemainderInteger = Math.round(MONTHS_IN_YEAR * monthsRemainderDecimal)
-
-const roundedYearsSinceWedding = monthsRemainderInteger < 12 ? yearsSinceWedding : yearsSinceWedding + 1
-const roundedMonthsRemainderInteger = monthsRemainderInteger < 12 ? monthsRemainderInteger : null
-
-const naturalLanguageString = `${roundedYearsSinceWedding} years` + (roundedMonthsRemainderInteger ? ` & ${roundedMonthsRemainderInteger} months` : "")
-
-console.log(roundedYearsSinceWedding, roundedMonthsRemainderInteger)
+const startDate = new Date(2016, 6, 22).getTime()
+const currentDate = new Date().getTime()
+const naturalLanguageString = getNaturalLanguageRange(startDate, currentDate)
 
 const calculatedRange = document.querySelector('#calculated-range')
 calculatedRange.textContent = naturalLanguageString
