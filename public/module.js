66aeb8c672d1dd2db0adca0f323ca2f986ee9322
Andrew Pyle <andrew.pyle@icloud.com>
2020-12-29 12:07:34 -0600

    Remove babel & use a `public/` folder

--- public/module.js
+++ public/module.js
@@ -4,14 +4,14 @@ const dateRangeDOMSelector = "#calculated-range";
 const dateRangeDOM = document.querySelector(dateRangeDOMSelector);
 
 if (dateRangeDOM) {
-  // Get Natural language date range
+  // Get Natural language date range & Replace placeholder with date
+  // duration string
   const WEDDING_DATE = new Date(2016, 6, 22).valueOf();
   const NOW = Date.now();
   const naturalLanguageString = createNaturalLanguageDuration({
     start: WEDDING_DATE,
     end: NOW,
   });
-  // Replace content with date duration string
   dateRangeDOM.textContent = naturalLanguageString;
 } else {
   throw new Error(
