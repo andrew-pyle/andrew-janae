cd6fb07722ac0fc394654526b7f0d1e2304e0dba
Andrew Pyle <andrew.pyle@icloud.com>
2022-07-25 10:53:18 -0500

    Use bundler (vite)

--- src/natural-lang-date-range.ts
+++ src/natural-lang-date-range.ts
@@ -1,7 +1,8 @@
-import {
-  intervalToDuration,
-  formatDuration,
-} from "https://cdn.skypack.dev/pin/date-fns@v2.16.1-kiAGbZM0AiQ3q2YJfvfc/min/date-fns.js";
+import { intervalToDuration, formatDuration } from "date-fns";
+// import {
+//   intervalToDuration,
+//   formatDuration,
+// } from "https://cdn.skypack.dev/pin/date-fns@v2.16.1-kiAGbZM0AiQ3q2YJfvfc/min/date-fns.js";
 
 /**
  * Converts a start date & end date into a description in English
@@ -11,7 +12,8 @@ import {
 export function createNaturalLanguageDuration({ start, end }) {
   const duration = intervalToDuration({ start, end });
   const dateString = formatDuration(duration, {
-    format: ["years", "months"],
+    format: ["years", "months", "weeks", "days"],
+    delimiter: ", ",
   });
   return dateString;
 }
