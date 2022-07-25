68da64df80f4d696879e82d828c13d15fe741354
Andrew Pyle <andrew.pyle@icloud.com>
2022-07-25 11:15:13 -0500

    Remove debugging console.log from script.

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,10 +2,9 @@ const appVersion = "2.0.0";
 const cacheName = `anniversary-${appVersion}`;
 
 // Delete old cache data
-self.addEventListener("activate", async function(e) {
+self.addEventListener("activate", async function (e) {
   const cache = await caches.open(cacheName);
   const cacheKeys = await cache.keys();
-  console.log(cacheKeys);
   for (const key of cacheKeys) {
     await cache.delete(key);
   }
